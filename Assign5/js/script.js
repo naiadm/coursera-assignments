73f33e083935faa439611f7656c2fac8fd22fb5d
Diana Baelly <dianabaelly@Dianas-Air.attlocal.net>
2021-05-01 17:47:14 -0700

    mod Assign 5

--- Assign5/js/script.js
+++ Assign5/js/script.js
@@ -115,9 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       
-      var chosenString = "'" + chosenCategoryShortName + "'";
-      console.log(chosenString);
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenString);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
